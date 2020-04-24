cfd4da43648b61ae735c3d97e65061fdb5308d94
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-04-24 12:23:23 +0200

    Use $PORT env var if available, fallback to 8080

--- src/main/scala/gol/WebServer.scala
+++ src/main/scala/gol/WebServer.scala
@@ -5,8 +5,13 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 
 import scala.concurrent.ExecutionContextExecutor
+import scala.util.Try
 
 object WebServer {
+
+  private val DefaultInterface = "0.0.0.0"
+  private val DefaultPort = 8080
+
   def main(args: Array[String]) {
 
     implicit val system: ActorSystem = ActorSystem()
@@ -21,9 +26,9 @@ object WebServer {
         handleWebSocketMessages(UserFlow.newUserFlow(system.actorOf(UserActor.props())))
       }
 
-    val interface = "0.0.0.0"
-    val port = 8080
-    Http().bindAndHandle(route, interface, port)
-    println(s"Server online at http://$interface:$port/")
+    val port = Option.apply(System.getenv("PORT")).flatMap(p => Try(p.toInt).toOption).getOrElse(DefaultPort)
+
+    Http().bindAndHandle(route, DefaultInterface, port)
+    println(s"Server online at http://$DefaultInterface:$port/")
   }
 }
