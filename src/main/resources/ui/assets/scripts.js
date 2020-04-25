34ace2a1fe027e5de30b986c04dc39079b86e8aa
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-04-25 20:28:26 +0200

    WIP

--- src/main/resources/ui/assets/scripts.js
+++ src/main/resources/ui/assets/scripts.js
@@ -1,100 +1,100 @@
-const WIDTH = 22
-const HEIGHT = 22
-const SATURATION = 0.5
-
-function createGrid(width, height, points) {
-  const parent = document.getElementById("world")
-  parent.textContent = '';
-
-  for (var i = 1; i <= height ; i++) {
-    for (var j = 1; j <= width; j++) {
-      var cell = document.createElement('div');
-      if (points && points.some(p => p.x === j && p.y === i)){
-        cell.classList.add("alive");
-      }
-      parent.appendChild(cell);
-    }
-  }
-}
-
-function populate(cells) {
-  createGrid(WIDTH, HEIGHT, cells);
-}
-
-function getSeed() {
-  return parseInt(document.getElementById("seed").textContent);
-}
-
-function getSpeed() {
-  return parseInt(document.getElementById("speed").textContent);
-}
-
-function getTick() {
-  return parseInt(document.getElementById("tick").textContent);
-}
-
-function updateTick(tick) {
-  document.getElementById("tick").textContent = `${tick}`;
-}
-
-function updateSeed(seed) {
-  document.getElementById("seed").textContent = `${seed}`;
-}
-
-function updateSpeed(speed) {
-  document.getElementById("speed").textContent = `${speed}`;
-}
-
-function send(s, event) {
-  s.send(`{"event": "${event}"}`);
-}
-
-function registerEditable(socket, id, fetch, update) {
-  document.getElementById(id).addEventListener("focus", (e) => {
-    send(socket, "pause");
-    const oldValue = fetch()
-
-    document.getElementById(id).addEventListener("blur", (e) => {
-      const currentValue = fetch()
-      if (Number.isInteger(currentValue)) {
-        if (oldValue != currentValue) {
-          const payload = {
-            width: WIDTH,
-            height: HEIGHT,
-            seed: getSeed(),
-            speed: getSpeed(),
-            saturation: SATURATION,
-            tick: getTick()
-          }
-          const data = {
-            event: "control",
-            payload: payload
-          }
-          socket.send(JSON.stringify(data));
-        }
-      } else {
-        update(oldValue) // default to old value
-      }
-    }, { once: true })
-  })
-  document.getElementById(id).addEventListener('keypress', (e) => {
-    if (e.which === 13) {
-      e.preventDefault();
-    }
-  });
-}
-
 window.onload = function(){
-
-  createGrid(WIDTH, HEIGHT);
-  document.getElementById("forward").onclick = () => send(s, "forward");
-  document.getElementById("back").onclick = () => send(s, "back");
-  document.getElementById("pause").onclick = () => send(s, "pause");
-  document.getElementById("play").onclick = () => send(s, "play");
-  document.getElementById("stop").onclick = () => send(s, "stop");
+  const WIDTH = 22
+  const HEIGHT = 22
+  const SATURATION = 0.5
 
   const protocol = location.hostname == "localhost" ? "ws" : "wss"
   const host = `${location.hostname}${location.port ? ':'+location.port: ''}`
+
+  var drawing = false
+  var color = "black"
+  var lastElement = undefined
+
+  function createGrid(width, height, points) {
+    const parent = document.getElementById("world")
+    parent.textContent = '';
+
+    for (var i = 1; i <= height ; i++) {
+      for (var j = 1; j <= width; j++) {
+        var cell = document.createElement('div');
+        cell.classList.add("cell");
+        if (points && points.some(p => p.x === j && p.y === i)){
+          cell.classList.add("alive");
+        } else {
+          cell.classList.add("dead");
+        }
+        parent.appendChild(cell);
+      }
+    }
+  }
+
+  function populate(cells) {
+    createGrid(WIDTH, HEIGHT, cells);
+  }
+
+  function getSeed() {
+    return parseInt(document.getElementById("seed").textContent);
+  }
+
+  function getSpeed() {
+    return parseInt(document.getElementById("speed").textContent);
+  }
+
+  function getTick() {
+    return parseInt(document.getElementById("tick").textContent);
+  }
+
+  function updateTick(tick) {
+    document.getElementById("tick").textContent = `${tick}`;
+  }
+
+  function updateSeed(seed) {
+    document.getElementById("seed").textContent = `${seed}`;
+  }
+
+  function updateSpeed(speed) {
+    document.getElementById("speed").textContent = `${speed}`;
+  }
+
+  function send(s, event) {
+    s.send(`{"event": "${event}"}`);
+  }
+
+  function registerEditable(socket, id, fetch, update) {
+    document.getElementById(id).addEventListener("focus", (e) => {
+      send(socket, "pause");
+      const oldValue = fetch()
+
+      document.getElementById(id).addEventListener("blur", (e) => {
+        const currentValue = fetch()
+        if (Number.isInteger(currentValue)) {
+          if (oldValue != currentValue) {
+            const payload = {
+              width: WIDTH,
+              height: HEIGHT,
+              seed: getSeed(),
+              speed: getSpeed(),
+              saturation: SATURATION,
+              tick: getTick()
+            }
+            const data = {
+              event: "control",
+              payload: payload
+            }
+            socket.send(JSON.stringify(data));
+          }
+        } else {
+          update(oldValue) // default to old value
+        }
+      }, { once: true })
+    })
+    document.getElementById(id).addEventListener('keypress', (e) => {
+      if (e.which === 13) {
+        e.preventDefault();
+      }
+    });
+  }
+
   const s = new WebSocket(`${protocol}:${host}/socket`);
   s.onopen = function(event) {
     const payload = {
@@ -111,6 +111,7 @@ window.onload = function(){
     }
     s.send(JSON.stringify(data));
   }
+
   s.onmessage = function (event) {
     if (event.data) {
       const world = JSON.parse(event.data)
@@ -119,8 +120,80 @@ window.onload = function(){
     }
   }
 
+  document.addEventListener('mousemove', function(e) {
+    if (drawing) {
+      const currentElement = document.elementFromPoint(e.pageX, e.pageY);
+      if (lastElement !== currentElement) {
+        lastElement = currentElement
+        if (currentElement.classList.contains("cell")) {
+          if (color === "white") {
+            currentElement.classList.remove('alive');
+            currentElement.classList.add('dead');
+          }
+          if (color === "black") {
+            currentElement.classList.remove('dead');
+            currentElement.classList.add('alive');
+          }
+        }
+      }
+    }
+  })
+
+  document.addEventListener('mousedown', function(e) {
+    const currentElement = document.elementFromPoint(e.pageX, e.pageY);
+    lastElement = currentElement
+    if (currentElement.classList.contains("cell")) {
+
+      if (color === "white") {
+        currentElement.classList.remove('alive');
+        currentElement.classList.add('dead');
+      }
+      if (color === "black") {
+        currentElement.classList.remove('dead');
+        currentElement.classList.add('alive');
+      }
+      // only start drawing in grid
+      send(s, "stop")
+      drawing = true
+    }
+  })
+
+  document.addEventListener('mouseup', function(e) {
+    drawing = false
+    lastElement = undefined
+  })
+
+  function clear(s) {
+    [...document.getElementsByClassName("cell")].forEach( c => {
+      c.classList.remove('alive');
+      c.classList.add('dead');
+    })
+    send(s, "stop");
+  }
+
+  function drawDead() {
+    color = "white"
+  }
+
+  function drawAlive() {
+    color = "black"
+  }
+
+  // initialize grid
+  createGrid(WIDTH, HEIGHT);
+
+  // register buttons
+  document.getElementById("forward").onclick = () => send(s, "forward");
+  document.getElementById("back").onclick = () => send(s, "back");
+  document.getElementById("pause").onclick = () => send(s, "pause");
+  document.getElementById("play").onclick = () => send(s, "play");
+  document.getElementById("stop").onclick = () => send(s, "stop");
+  document.getElementById("clear").onclick = () => clear(s);
+  document.getElementById("white").onclick = () => drawDead();
+  document.getElementById("black").onclick = () => drawAlive();
+
+  // regist editables
   registerEditable(s, "tick", getTick, updateTick)
   registerEditable(s, "seed", getSeed, updateSeed)
   registerEditable(s, "speed", getSpeed, updateSpeed)
-
 }
